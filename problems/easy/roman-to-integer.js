02f8dbb576f38985328dd665066a4d916719b32e
marcos-j-ferreira <marcos.j.lemes.2004@gamil.com>
2024-11-16 22:25:12 -0300

    roman-to-integer

--- problems/easy/roman-to-integer.js
+++ problems/easy/roman-to-integer.js
@@ -12,4 +12,33 @@ M             1000
 
 var romanToInt = function(s) {
     
+    var letter = s.split("")
+    var result = 0;
+
+    for(var i =0; i < letter.length; i++){
+
+        if(letter[i] == 'I'){
+            result = result + 1
+        }else if(letter[i] == 'V'){
+            result = result + 5
+        }else if(letter[i] == 'X'){
+            result = result + 10
+        }else if(letter[i] == 'L'){
+            result = result + 50
+        }else if(letter[i] == 'C'){
+            result = result + 100
+        }else if(letter[i] == 'D'){
+            result = result + 500
+        }else if(letter[i] == 'M'){
+            result = result + 1000
+        }
+    }
+    return result
 };
+
+var s = "MCMXCIV"
+
+var print = romanToInt(s)
+
+
+console.log(print)
