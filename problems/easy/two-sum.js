31037c43df5c78988a2b57e1707f7f4603e6dfa5
marcos-j-ferreira <marcos.j.lemes.2004@gamil.com>
2024-11-16 22:12:08 -0300

    roman-to-integer

--- problems/easy/two-sum.js
+++ problems/easy/two-sum.js
@@ -2,16 +2,13 @@ var twoSum = function(nums, target) {
     
 
     var size = nums.length  
-
     let result = []
 
 
     for(var i = 0; i <= size; i++){
         for(var j = i + 1; j < size ; j++){
 
-            
             var sum = nums[i] + nums[j]
-            
             if(sum == target){
                 result.push(i, j)
                 return result
